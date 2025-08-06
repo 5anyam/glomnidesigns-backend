fe43622348a7b20befcd1f3f50888ceadf24063d
Sanyam <you@example.comsanyamgulia@gmail.com>
2025-08-06 16:37:51 +0530

    Add new files

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,18 @@
-export default () => ({});
+// config/plugins.js
+const Crypto = require('crypto');
+
+module.exports = ({ env }) => ({
+  'users-permissions': {
+    config: {
+      jwtSecret: env('JWT_SECRET') || 'defaultJWTSecretForDevelopment123',
+      jwt: {
+        expiresIn: '30d',
+      },
+    },
+  },
+  upload: {
+    config: {
+      // Upload configuration if needed
+    },
+  },
+});
