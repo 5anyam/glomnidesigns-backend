1eab8e5e934533f36922efd747256a145daba7bc
Sanyam <you@example.comsanyamgulia@gmail.com>
2025-08-06 16:30:54 +0530

    Add new files

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,13 @@
-export default ({ env }) => ({
+// config/server.js
+module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS') || [
+      'myapp-key-1',
+      'myapp-key-2',
+      'myapp-key-3',
+      'myapp-key-4'
+    ],
   },
 });
